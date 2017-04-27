3bf383a9fb2cca3d3933d6e9adc8558ef69a7bbc
tqhung04 <tqhung04@gmail.com>
2017-04-28 00:59:08 +0700

    Add favicon

--- resources/views/Admin/zlayouts/index.blade.php
+++ resources/views/Admin/zlayouts/index.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
 
+    <link rel="shortcut icon" href="{{{ asset('img/favicon.ico') }}}">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
